08ff598553901d0d2c07576d1236d576ffc8fec5
Brandon Neth <brandon.neth@hpe.com>
2024-08-26 15:49:04 -0700

    changed the use cases from testimonials to applications in the backend. --- Signed-off-by: Brandon Neth <brandon.neth@hpe.com>

--- themes/hugo-universal-theme/static/js/front.js
+++ themes/hugo-universal-theme/static/js/front.js
@@ -105,11 +105,11 @@ function sliders () {
       itemsMobile: [480, 1]
     })
 
-    $('.testimonials').owlCarousel({
-      items: ($('.testimonials').attr('data-items') || 4),
-      slideSpeed: ($('.testimonials').attr('data-slide-speed') || 2000),
-      paginationSpeed: ($('.testimonials').attr('data-pagination-speed') || 1000),
-      autoPlay: $('.testimonials').attr('data-autoplay') === 'true',
+    $('.applications').owlCarousel({
+      items: ($('.applications').attr('data-items') || 4),
+      slideSpeed: ($('.applications').attr('data-slide-speed') || 2000),
+      paginationSpeed: ($('.applications').attr('data-pagination-speed') || 1000),
+      autoPlay: $('.applications').attr('data-autoplay') === 'true',
       itemsDesktopSmall: [990, 3],
       itemsTablet: [768, 2],
       itemsMobile: [480, 1]
